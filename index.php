efb3395e6b0b04c98c251830b3e87e4f30ee6513
Sravani565 <82518509+Sravani565@users.noreply.github.com>
2022-04-08 19:32:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| Online Career Counselling System |</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/book.png) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                <h1> online career counselling system </h1>
+                <a href="login.php" class="btn"> login </a> &emsp;
+                <a href="register.php" class="btn"> register </a>
+                <h2> Good &nbsp;Luck. </h2>
+			
+				<footer><h2>Created by MECS students[1608-19-733-085,1608-19-733-096,1608-19-733-102]</h2></footer>
+            </div>
+        </center>
+		
+		
+    </body>
+</html>
